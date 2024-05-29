83da23e43d125c8f19ec4d1091a83b20babbf772
amonry <edinsoncode@gmail.com>
2024-05-29 18:03:37 +0300

    blog remove

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/ShibGPT',
             label: 'GitHub',
